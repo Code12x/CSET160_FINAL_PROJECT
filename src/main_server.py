4082885041954d6e038ac7589efa5396916b5fb7
Code12x <87677172+Code12x@users.noreply.github.com>
2023-04-09 09:46:10 -0400

    progress

--- src/main_server.py
+++ src/main_server.py
@@ -1,8 +1,10 @@
+from datetime import datetime, timedelta
 from functools import wraps
 import jwt
 import pymysql
 from environs import Env
-from flask import Flask, render_template, request
+from flask import Flask, make_response, render_template, request, redirect, session
+from os import urandom
 
 # Environmental variables
 env = Env()
@@ -22,37 +24,110 @@ app.config['SECRET_KEY'] = env("SECRET_KEY")
 connection = pymysql.connect(host=MYSQL_HOST, port=MYSQL_PORT,
                              user=MYSQL_USERNAME, password=MYSQL_PASSWORD, database=MYSQL_DATABASE)
 
+tokens = []
 
-def access_token_required(func):
-    @wraps(func)
-    def decorated(*args, **kwargs):
-        if not has_access_token():
-            app.redirect("/login")
-        return func(*args, **kwargs)
-    return decorated
+# def access_token_required(func):
+#     @wraps(func)
+#     def decorated(*args, **kwargs):
+#         if not has_access_token():
+#             app.redirect("/login")
+#         return func(*args, **kwargs)
+#     return decorated
 
 
-def has_access_token():
-    """Returns True if there is a valid JWT Token in the head, and it sets request.user = payload.get("user").
-    Returns False if there are no JWT tokens provided or if the JWT token is invalid"""
-    token = request.headers.get("authorization")
-    if not token:
-        return False
+def validate_token(token, refresh=False):
+    try:
+        payload = jwt.decode(token, app.config['SECRET_KEY'],
+                             algorithms=["HS256"], verify_signature=True)
+        return {"status": "success", "payload": payload}
+    except jwt.exceptions.InvalidTokenError:
+        return {"status": "invalid"}
+    except:
+        return {"status": "error"}
+
+
+def try_set_user(refresh=False):
+    if refresh:
+        token = request.cookies.get("refresh_token")
     else:
-        try:
-            token = token.split(" ")[1]
-            payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=[
-                                 "HS256"], verify_signature=True)
-            print("\n\n--------------------\n" +
-                  payload.get("user") + "\n----------------\n\n")
-            request.user = payload.get("user")
-            return True
-        except jwt.exceptions.InvalidTokenError:
-            print("Invalid Token Error")
-            return False
-        except jwt.exceptions.DecodeError:
-            print("DecodeError")
-            return False
+        token = request.args.get("token")
+
+    if not token:
+        return {"status": "missing"}
+    else:
+        token_status = validate_token()
+
+        if token_status.status == "success":
+            request.user = token_status.payload.user
+            return {"status": "success"}
+
+        elif token_status.status == "invalid":
+            if not refresh:
+                try_set_user(refresh=True)
+
+            return {"status": "invalid"}
+
+        if payload:
+            request.user = payload.user
+
+         if not refresh:
+            try:
+                if try_set_token(refresh=True).get("status") == "valid":
+
+
+def generate_token(user, refresh=False):
+    payload = {'user': user}
+    if refresh:
+        payload['salt'] = urandom(16).hex()
+    else:
+        payload['exp'] = datetime.utcnow() + 60*10
+
+    token = jwt.encode(payload, app.config['SECRET_KEY'])
+    return token
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        if request.form['username'] and request.form['password'] == '123456':
+            session['logged_in'] = True
+
+            refresh_token = generate_token(
+                request.form['username'], refresh=True)
+            access_token = generate_token(request.form["username"])
+
+            res = make_response({
+                "access_token": access_token,
+            })
+            res.set_cookie("refresh_token", refresh_token)
+            return res
+
+        else:
+            return make_response('Unable to verify', 403,
+                                 {'WWW-Authenticate': 'Basic realm: "Authentication Failed!"'})
+    elif request.method == "GET":
+        return render_template("login.html")
+
+
+@ app.route('/logout', methods=["DELETE"])
+def logout():
+    refresh_token = request.headers.get("refresh_token")
+    if not refresh_token:
+        return make_response({"logout_status": "missing_token"})
+    else:
+        if try_set_token(refresh=True):
+            return make_response({"logout_status": "success"})
+        else:
+            return make_response({"logout_status": "invalid"})
+
+
+@app.route("/token", methods=["POST"])
+def token():
+    refresh_token_status = try_set_token()
+    if refresh_token_status.get("status") == "valid":
+        return make_response({"access_token": generate_token(request.user)})
+    else:
+        return app.redirect("/login")
 
 
 @app.route("/")
@@ -67,32 +142,34 @@ def register():
 
 
 @app.route("/cool")
-@access_token_required
 def cool():
-    return render_template("cool.html", name=request.user)
+    if has_access_token():
+        return render_template("cool.html", name=request.user)
+    else:
+        return redirect("/login")
 
 
 @app.route("/users")
-@access_token_required
+# @access_token_required
 def users():
     # use request.args.get('filter')
     pass
 
 
 @app.route("/tests")
-@access_token_required
+# @access_token_required
 def tests():
     pass
 
 
 @app.route("/test/take/<int:test_id>", methods=["GET", "POST"])
-@access_token_required
+# @access_token_required
 def test_take_get(test_id):
     pass
 
 
 @app.route("/test/create/<int:test_id>", methods=["GET", "POST", "UPDATE"])
-@access_token_required
+# @access_token_required
 def test_create_get(test_id):
     pass
 
--- src/static/scripts/authorization.js
+++ src/static/scripts/authorization.js
@@ -1,17 +1,55 @@
-links_to_add_access_token = document.getElementsByClassName("add_access_token");
-for (i = 0; i < links_to_add_access_token.length - 1; i++) {
+links_to_add_access_token = document.getElementsByClassName("add_access_token_to_href");
+for (i = 0; i < links_to_add_access_token.length; i++) {
     link = links_to_add_access_token[i];
     link.addEventListener('click', (e) => {
         e.preventDefault();
 
-        const headers = { 'Authorization': 'Bearer ' + localStorage.getItem("access_token") };
+        let url = new URL(link.getAttribute("href"));
+        url.searchParams.append("token", localStorage.getItem("access_token"));
 
-        fetch(link.getAttribute('href'), { headers })
-            .then(response => {
-                window.location.href = response.url;
-            })
-            .catch(error => {
-                console.error('Request failed:', error);
-            });
+        location.assign(url.toString());
     })
 }
+
+
+let logout_form = document.getElementById("logout_form");
+logout_form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let req = new XMLHttpRequest();
+
+    req.open("DELETE", "/logout");
+
+    req.setRequestHeader("refresh_token", localStorage.getItem("refresh_token"));
+
+    req.onload = () => {
+        res = JSON.parse(req.responseText);
+        if (res.logout_status === "success") {
+            localStorage.removeItem("refresh_token");
+            localStorage.removeItem("access_token");
+            location.assign("/");
+        } else {
+            console.log("Error logging out: " + res);
+        }
+    };
+    req.send();
+});
+
+
+function renew_access_token() {
+    req = new XMLHttpRequest();
+
+    req.open("POST", "/token");
+
+    req.setRequestHeader("refresh_token", localStorage.getItem("refresh_token"));
+
+    req.onload = () => {
+        res = JSON.parse(req.responseText);
+        localStorage.setItem("access_token", res.access_token);
+    };
+    req.send();
+}
+
+
+
+
