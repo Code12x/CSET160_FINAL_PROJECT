d77095a2d8abb9fb94107ae5f71ae2a5b43f6947
Code12x <87677172+Code12x@users.noreply.github.com>
2023-04-10 19:09:07 -0400

    progress update

--- src/website/views.py
+++ src/website/views.py
@@ -15,11 +15,32 @@ def index():
 @login_required
 def create_test():
     if not current_user.is_teacher:
-        return redirect(url_for("login"))
+        return redirect(url_for("tests"))
 
     if request.method == "POST":
-        teacher_id = db.session.execute(
-            text(f"SELECT teacher_id FROM Teachers WHERE user_id={current_user.user_id}"))
+        teacher_id = Teachers.query.filter_by(
+            user_id=current_user.user_id).first().teacher_id
+        test_name = request.form.get("name")
+        q1 = request.form.get("q1")
+        q2 = request.form.get("q2")
+        q3 = request.form.get("q3")
+        a1 = request.form.get("a1")
+        a2 = request.form.get("a2")
+        a3 = request.form.get("a3")
+
+        test = Tests(teacher_id=teacher_id, test_name=test_name)
+        db.session.add(test)
+        db.session.commit()
+
+        test_question1 = TestQuestions(
+            test_id=test.test_id, question=q1, answer=a1)
+        test_question2 = TestQuestions(
+            test_id=test.test_id, question=q2, answer=a2)
+        test_question3 = TestQuestions(
+            test_id=test.test_id, question=q3, answer=a3)
+        db.session.add_all([test_question1, test_question2, test_question3])
+        db.session.commit()
+        return redirect(url_for("views.tests"))
 
     return render_template("create_test.html")
 
@@ -27,7 +48,12 @@ def create_test():
 @views.route("/tests")
 @login_required
 def tests():
-    return render_template("tests.html")
+    if current_user.is_teacher:
+        teacher_id = Teachers.query.filter_by(user_id=current_user.user_id)
+        tests = Tests.query.fiter_by(teacher_id=teacher_id)
+    else:
+        tests = Tests.query.all()
+    return render_template("tests.html", test=tests)
 
 
 @views.route("/users")
